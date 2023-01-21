c6e78abe6b04ec58c5c2444c0bce30a2cc872ac5
almog5690 <almog.yair@mail.huji.ac.il>
2023-01-21 15:23:52 +0200

    Update BASiCSAnalysis.R

--- Code/BASiCSAnalysis.R
+++ Code/BASiCSAnalysis.R
@@ -3,10 +3,12 @@ library(rhdf5)
 library(BASiCS)
 
 BASiCS_analysis <- function(data.type){
+  # Set data directories: 
+  set_data_dirs(data.type)
   samples <- get_tissue_file_names(data.type)
   meta.data = get_meta_data(data.type)
-  
-  
+  groups = dataset_to_age_groups(data.type)
+
   
   for(i in 1:length(samples$organs)){  # for(i in 1:2){
     read.file <- paste0(processed.data.dir, samples$organs[i], ".", processed.files.str[data.type], ".rds")
@@ -14,6 +16,7 @@ BASiCS_analysis <- function(data.type){
     SC = readRDS(file = read.file) # Current tissue seurat object
     counts.mat = as.matrix(SC@assays$RNA@data) # the data matrix for the current tissue
     list2env(tissue_to_age_inds(data.type, samples$organs[i], groups, SC@meta.data), env=environment()) # set specific ages for all age groups in all datasets
+    n_cell_types = max(SC@meta.data$cell.ontology.class) # number of cell types
     
     SC_gene_name = toupper(rownames(SC)) # Gene names (in uppercase)
     if(length(SC_gene_name) != dim(counts.mat)[1]) { # For Rats, names are already in the matrix
@@ -30,12 +33,26 @@ BASiCS_analysis <- function(data.type){
       cell_types_categories = meta.data[[i]]$cell_ontology_class # Cell type names. Missing variable meta.data.drop
     }
     
-    for(CT_ind in cells_ind){ # loop on cell types . 
-      print(paste0("Analyze cell type: ", cell_types_categories[k]))
+    earase = vector()
+    # filltering the cell-types
+    for(ct_ind in 0:n_cell_types){
+      #filtering cell types with less then 100 cells or less the 20 cells in each the age groups
+      if(sum(cell_types==ct_ind)<100 | sum((cell_types==ct_ind)&(young.ind)) < 20 | sum((cell_types==ct_ind)&(!young.ind)) < 20){
+        earase = c(earase,ct_ind+1)
+        next()
+      }
+      
+    }
+    # the vector of filltered cell-types
+    cells_ind = c(1:(n_cell_types+1))[-earase]
+    if(length(cells_ind) == 0) cells_ind = (1:(n_cell_types+1))
+    
+    for(ct_ind in cells_ind){ # loop on cell types . 
+      print(paste0("Analyze cell type: ", cell_types_categories[ct_ind]))
       
       # Filter genes with less then 10 reads for either age group
-      old_sum = rowSums(counts.mat[,(cell_types==ct_ind & old.ind)]) 
-      young_sum = rowSums(counts.mat[,(cell_types==ct_ind & young.ind)])
+      old_sum = rowSums(counts.mat[,(cell_types==(ct_ind-1) & old.ind)]) 
+      young_sum = rowSums(counts.mat[,(cell_types==(ct_ind-1) & young.ind)])
       expressed_genes = which(old_sum > 10 & young_sum > 10)
       
       if(data.type == "CR.Rat"){
@@ -48,34 +65,34 @@ BASiCS_analysis <- function(data.type){
       if(data.type == "CR.Rat"){
         basics.chains.dir =  paste0(main.data.dir, 'Data/CR.Rat/chains/')
         basics.DVT.dir =  paste0(main.data.dir, 'Data/CR.Rat/DVT/')
-        DVT.file.name = paste0(basics.DVT.dir,paste(samples$organs[i],cell_types_categories[ct_ind + 1],"same-mean.RData"))
+        DVT.file.name = paste0(basics.DVT.dir,paste(samples$organs[i],cell_types_categories[ct_ind],"same-mean.RData"))
 
       }
       if(data.type == "TM.droplet"){
-        basics.chains.dir =  paste0(main.data.dir, 'Data/TabulaMuris/chains/droplet')
-        basics.DVT.dir =  paste0(main.data.dir, 'Data/TabulaMuris/DVT/droplet')
-        DVT.file.name = paste0(basics.DVT.dir,paste(samples$organs[i],cell_types_categories[ct_ind + 1],"drop 3-24 same-mean.RData"))
+        basics.chains.dir =  paste0(main.data.dir, 'Data/TabulaMuris/chains/TM.droplet')
+        basics.DVT.dir =  paste0(main.data.dir, 'Data/TabulaMuris/DVT/TM,droplet')
+        DVT.file.name = paste0(basics.DVT.dir,paste(samples$organs[i],cell_types_categories[ct_ind],"drop 3-24 same-mean.RData"))
       }
       if(data.type == "TM.facs"){
-        basics.chains.dir =  paste0(main.data.dir, 'Data/TabulaMuris/chains/facs')
-        basics.DVT.dir =  paste0(main.data.dir, 'Data/TabulaMuris/DVT/facs')
-        DVT.file.name = paste0(basics.DVT.dir,paste(samples$organs[i],cell_types_categories[ct_ind + 1],"same-mean.RData"))
+        basics.chains.dir =  paste0(main.data.dir, 'Data/TabulaMuris/chains/TM.facs')
+        basics.DVT.dir =  paste0(main.data.dir, 'Data/TabulaMuris/DVT/TM.facs')
+        DVT.file.name = paste0(basics.DVT.dir,paste(samples$organs[i],cell_types_categories[ct_ind],"same-mean.RData"))
       }
       
       # We preform the BASiCS only if both age group have more then 1 mouse  
-      if(!(length(table(batch[cell_types == ct_ind & young.ind]))==1|length(table(batch[cell_types == ct_ind & old.ind]))==1)){ 
+      if(!(length(table(batch[cell_types == (ct_ind-1) & young.ind]))==1|length(table(batch[cell_types == (ct_ind-1) & old.ind]))==1)){ 
         
         # BASiCS data for old and young mice  
-        old_bs = newBASiCS_Data(Counts = counts.mat[expressed_genes,cell_types == ct_ind & old.ind],BatchInfo = batch[cell_types == ct_ind & old.ind]) 
-        young_bs = newBASiCS_Data(Counts = counts.mat[expressed_genes,cell_types == ct_ind & young.ind],BatchInfo = batch[cell_types == ct_ind & young.ind]) 
+        old_bs = newBASiCS_Data(Counts = counts.mat[expressed_genes,cell_types == (ct_ind-1) & old.ind],BatchInfo = batch[cell_types == (ct_ind-1) & old.ind]) 
+        young_bs = newBASiCS_Data(Counts = counts.mat[expressed_genes,cell_types == (ct_ind-1) & young.ind],BatchInfo = batch[cell_types == (ct_ind-1) & young.ind]) 
         
         # Creating BASiCS chain for old and young mice
         chain_old = BASiCS_MCMC(Data = old_bs,N = 20000,Thin = 20,Burn = 10000,Regression = T,
                                 WithSpikes = F,PrintProgress = FALSE, StoreChains = TRUE, StoreDir = basics.chains.dir,
-                                RunName = paste(samples$organs[i],cell_types_categories[k],data.type,"old")) 
+                                RunName = paste(samples$organs[i],cell_types_categories[(ct_ind-1)],data.type,"old")) 
         chain_young = BASiCS_MCMC(Data = young_bs,N = 20000,Thin = 20,Burn = 10000,Regression = T,
                                   WithSpikes = F,PrintProgress = FALSE, StoreChains = TRUE, StoreDir = basics.chains.dir,
-                                  RunName = paste(samples$organs[i],cell_types_categories[k],data.type,"young"))  
+                                  RunName = paste(samples$organs[i],cell_types_categories[(ct_ind-1)],data.type,"young"))  
         
         # Differential over-dispersion test - only on genes without significant difference in mean expression
         test = BASiCS_TestDE(Chain1 = chain_old,Chain2 = chain_young,GroupLabel1 = "Old",
