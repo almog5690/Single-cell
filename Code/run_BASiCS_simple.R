e0927e5c8a28cfdd22f35b478aa6eabdf0a019cf
orzuk <or.zuk@mail.huji.ac.il>
2023-08-01 14:20:44 +0300

    Added simple BASiCS script

--- Code/run_BASiCS_simple.R
+++ Code/run_BASiCS_simple.R
@@ -1,10 +1,11 @@
 # Simple example of running BASiCS
-
+data.type = "TM.facs"  # Blood_SC
+organ = "Aorta" # Blood (?) # smallest facs file, should be fastest 
 if(!exists(user.name))  # set default values 
   user.name = "Unix" #  "Almog"  # Or # Unix 
 if(!exists(data.type))  # set default values 
   data.type = "TM.facs"  # Choose one type for analysis (can later loop over multiple datasets)
-organ = "Aorta" # smallest facs file, should be fastest 
+source("scRNA_seq_config.R")
 print("Running BASiCS:")
 BASiCS_analysis_tissue(data.type, organ)
 print("Finished Running BASiCS!")
