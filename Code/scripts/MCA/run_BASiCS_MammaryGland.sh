5619003dbb49eb30f3bd27585d646f896994b80d
orzuk <or.zuk@mail.huji.ac.il>
2024-01-18 15:23:19 +0200

    Added BASICS scripts

---
+++ Code/scripts/MCA/run_BASiCS_MammaryGland.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --time=168:00:00
+#SBATCH --ntasks=4
+#SBATCH --mem=48G
+module load R4
+
+#!/usr/bin/env Rscript
+
+module load R4
+
+Rscript --vanilla ../run_BASiCS_commandline.R MCA MammaryGland
